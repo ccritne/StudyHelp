58293a21f6f0dcf36c613b80d5db4c955e5e82cc
Marco Secci <marcosecci2002@gmail.com>
2023-11-03 01:17:11 +0100

    corrected typo, added logging

--- tkinter/calc_deadline.py
+++ tkinter/calc_deadline.py
@@ -1,7 +1,14 @@
 from tk_setup import WEEKDAYS
 from datetime import datetime as dt, timedelta as td
 from termcolor import colored
+import logging as log
 
+# Create log for this script:
+log.basicConfig(
+    filename="logs/calc_deadline.log",
+    filemode="w",
+    format="%(name)s - %(levelname)s - %(message)s",
+)
 
 today_index = 0
 pages_left = 0
@@ -41,11 +48,11 @@ def calculate_deadline(
     studied_pages: int | None = 0,
 ) -> dt | str:
     """
-    # `calculate_deadline` method
+    ## `calculate_deadline` method
 
     ===============================================
 
-    ### `Description`
+    #### `Description`
 
     This method calculates the deadline for the completion of a task like studying or exercising. \n
     The calculations are made taking into account how many study hours are available, how many pages
@@ -53,51 +60,51 @@ def calculate_deadline(
 
     ===============================================
 
-    ### `Parameters`
+    #### `Parameters`
 
-    ##### @param `list_session_week`: `list`
+    #####- `list_session_week`: `list`
 
     This list contains the weekly study plan. \n
     It is stored in db as `arr_session` and it depends on the user. \n
-    It's created when a new source is inserted and his structure is: \n
+    It's created when a new source is inserted and its structure is: \n
     {
-        'total_pages': int, # sum of each total_pages of all weekdays \n
-        'total_minutes': int, # For the future \n
-        'total_duration': int, # sum of each total_duration of all weekdays \n
-        'is_book': True/False, \n
-        for x of WEEKDAYS: \n
-            x:{ \n
-                'is_study_day': True/False, # Check if x the user study (in general) \n
-                'are_there_sessions: True/False, # Check if the user inserted sessions for this source \n
-                'amount': int, # Numbers of sessions \n
-                'types': list, # His size is the amount, an element can be one of these ['Reading', 'Schematization', 'Testing'] \n
-                'durations': list, # His size is the amount, an element is an int between 0 and 59 (the lenght in minutes of the session) \n
-                'pages': list, # His size is the amount, an element is an int greater or equal 0 and corresponds to the pages of each sessions \n
-                'total_pages': int, # sum(pages) \n
-                'total_duration': int, # sum(durations) \n
-                'are_there_lectures': True/False, # Check if x the user follows a lecture for this source \n
-                'time_lectures': { \n
-                    'time_start_date_lecture': "XX:XX", # The start of the lecture \n
-                    'time_end_date_lecture': "XX:XX", # The end of the lecture \n
-                    'duration_lecture': int, # Timedelta in minutes, time_end_date_lecture - time_start_date_lecture \n
+        `'total_pages'`: `int`, sum of each total_pages of all weekdays \n
+        `'total_minutes'`: `int`, For the future \n
+        `'total_duration'`: `int`, sum of each total_duration of all weekdays \n
+        `'is_book'`: `True`/`False`, \n
+        for `x` of WEEKDAYS: \n
+            `x`:{ \n
+                `'is_study_day'`: `True`/`False`, Check if x the user study (in general) \n
+                `'are_there_sessions'`: True/False, Check if the user inserted sessions for this source \n
+                `'amount'`: `int`, Numbers of sessions \n
+                `'types'`: list, Its size is the amount, an element can be one of these ['Reading', 'Schematization', 'Testing'] \n
+                `'durations'`: list, Its size is the amount, an element is an `int` between 0 and 59 (the length in minutes of the session) \n
+                `'pages'`: list, Its size is the amount, an element is an `int` greater or equal 0 and corresponds to the pages of each sessions \n
+                `'total_pages'`: `int`, sum(pages) \n
+                `'total_duration'`: `int`, sum(durations) \n
+                `'are_there_lectures'`: True/False, Check if x the user follows a lecture for this source \n
+                `'time_lectures'`: { \n
+                    `'time_start_date_lecture'`: "XX:XX", The start of the lecture \n
+                    `'time_end_date_lecture'`: "XX:XX", The end of the lecture \n
+                    `'duration_lecture'`: `int`, Timedelta in minutes, time_end_date_lecture - time_start_date_lecture \n
                 } \n
             } \n
-        "with_lectures": True/False, # Check if the user has associated lectures with the study of the source \n
-        "week_reps_lectures": "BBBBBBB", # B can be 0 or 1. The i-th B says if the user inserted a lecture in i-th day of the week. \n
-        "start_date_lectures": "XXXX-XX-XX", # The start date of the lectures \n
-        "end_date_lectures": "XXXX-XX-XX", # The end date of the lecture \n
+        `"with_lectures"`: True/False, Check if the user has associated lectures with the study of the source \n
+       `"week_reps_lectures"`: `"BBBBBBB"`, B can be `0` or `1`. The i-th B says if the user inserted a lecture in i-th day of the week. \n
+        `"start_date_lectures"`: `"XXXX-XX-XX"`, The start date of the lectures \n
+        `"end_date_lectures"`: `"XXXX-XX-XX"`, The end date of the lecture \n
     } \n
 
 
-    ##### @param `is_book`: `bool = True`
+    #####- `is_book`: `bool = True`
 
-    With a default value of True, this parameter checks if the source is a book or other (video, audio).
+    This parameter checks if the source is a book or other type (video, audio).
 
-    ##### @param `total_pages`: `int | None = 0`
+    #####- `total_pages`: `int | None = 0`
 
     Number of pages needed to complete the source.
 
-    ##### @param `studied_pages`: `int | None = 0`
+    #####- `studied_pages`: `int | None = 0`
 
     Number of pages already studied by the user.
     """
@@ -125,31 +132,22 @@ def calculate_deadline(
     # Adding the calculated days to the deadline:
     deadline += td(days=days_to_add)
 
-    # ?[Closed][@marco-secci][seen] QUESTION:
-    # Idk what this does, still have to understand what exactly will be list_session_week
-    #
-    # ! ANSWER (@ccritne):
-    # It's the total pages (or minutes) of source during the week.
+    # Total pages (or minutes) of source during the week.
     # In the future index_str can be 'total_pages' or 'total_minutes'.
     # At the moment exists only the book version.
-    # list_session_week is explained in the parameter of this function.
-    week_do = list_session_week[index_str]
+    week_todo = list_session_week[index_str]
 
     try:
-        # ?[Closed][@marco-secci][to-see] QUESTION:
-        # same as above
-        #
-        # ! ANSWER (@ccritne):
-        # It's the remaining week to complete the book.
-        # If it is greater than 0 I sub 1 to have the exact day.
-        weeks_add = int(pages_left / week_do)
+        # Remaining week to complete the book; if it is greater than 0,
+        # the value is reduced by 1 to have the exact day.
+        weeks_add = int(pages_left / week_todo)
 
         if weeks_add > 0:
             weeks_add -= 1
 
         # Calculating how many days are needed to be added from the number of weeks.
         days_add = weeks_add * 7
-        pages_left -= week_do * weeks_add
+        pages_left -= week_todo * weeks_add
         deadline += td(days=days_add)
 
         today_index = 0
@@ -167,10 +165,15 @@ def calculate_deadline(
         # Adding time to deadline:
         deadline += td(days=days_to_add)
 
+        # Logging before returning:
+        log.info(f"{dt.now}: Deadline calculated successfully. ")
+
         # Final output will be a datetime object:
         return deadline
 
     except ZeroDivisionError as e:
+        # Logging before returning:
+        log.info(f"{dt.now}: An ERROR occurred: {e}")
         return "Insert sessions!"
 
 
@@ -232,7 +235,7 @@ def TestDeadline():
     else:
         print(colored("Test. '100 pages no planned' success!", "green"))
 
-    hundread_pages_monday_input = {
+    hundred_pages_monday_input = {
         "total_pages": 100,
         "total_minutes": 0,
         "total_duration": 55,
@@ -287,7 +290,7 @@ def TestDeadline():
         day=6, month=11, year=2023
     ).date()  # Test for 2023-11-02
     pages_100_plan_all_in_monday_true_result = calculate_deadline(
-        list_session_week=hundread_pages_monday_input, total_pages=100
+        list_session_week=hundred_pages_monday_input, total_pages=100
     )
     if (
         pages_100_plan_all_in_monday_ideal_result
